08c3b09da8d8d2dbcc3432a6ed6a1afc4b58ba99
Collin Chapman <cchap14@gmail.com>
2021-02-13 08:49:23 -0800

    create test number 1, if user inputs 1 return Beep

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+// Business Logic
+
+const userNumber = 1
+const outputArray = [];
+
+function beepBoop(userNumber) {
+  for (let i =0; i <= userNumber; i ++) {
+    if (i === 1) {
+      outputArray.push("Beep!");
+    } else {
+    }
+    }
+  }
+
+beepBoop(userNumber)
