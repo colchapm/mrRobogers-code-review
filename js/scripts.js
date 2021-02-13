504c7dfb610e4232cdf5ee85f5ec19698beb0ad6
Collin Chapman <cchap14@gmail.com>
2021-02-13 08:56:20 -0800

    create test number 2, if user enters 2 return Boop

--- js/scripts.js
+++ js/scripts.js
@@ -6,13 +6,13 @@
 
 // Business Logic
 
-const userNumber = 1
+const userNumber = 2
 const outputArray = [];
 
 function beepBoop(userNumber) {
   for (let i =0; i <= userNumber; i ++) {
-    if (i === 1) {
-      outputArray.push("Beep!");
+    if (i === 2) {
+      outputArray.push("Boop!");
     } else {
     }
     }
